2b2945cb8ffb0757ac8493abc270445d1425dda4
icekiller87 <89640225+icekiller87@users.noreply.github.com>
2026-08-31 22:27:31 -0300

    Update lucinda-lead.mjs
    front att

--- netlify/functions/lucinda-lead.mjs
+++ netlify/functions/lucinda-lead.mjs
@@ -10,7 +10,7 @@ const escapeHtml = (value) => String(value).replace(/[&<>'"]/g, (char) => ({
 })[char]);
 
 const allowedLives = new Set(["1 pessoa", "2 pessoas", "3 a 5 pessoas", "6 a 29 pessoas", "30 ou mais pessoas"]);
-const allowedPlans = new Set(["Individual ou familiar", "Empresarial / PME", "Melhor idade", "Odontológico", "Quero ajuda para decidir"]);
+const allowedPlans = new Set(["Individual ou familiar", "Empresarial / PME", "Melhor idade", "Odontológico", "Plano Pet", "Quero ajuda para decidir"]);
 const validBrazilianMobile = /^(?:11|12|13|14|15|16|17|18|19|21|22|24|27|28|31|32|33|34|35|37|38|41|42|43|44|45|46|47|48|49|51|53|54|55|61|62|63|64|65|66|67|68|69|71|73|74|75|77|79|81|82|83|84|85|86|87|88|89|91|92|93|94|95|96|97|98|99)9\d{8}$/;
 
 
